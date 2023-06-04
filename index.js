c22323e5d40d431998dd652ba42d0632e997d4e3
NicolasAlvarez9915 <nicoalvarez9915@gmail.com>
2023-06-03 22:35:15 -0500

    Cors habilitados

--- index.js
+++ index.js
@@ -4,15 +4,12 @@ const Connection = require('./database');
 const app = express();
 const {PORT} = require('./config.js');
 const routes = require('./routes');
-const NodoControllers = require('./controllers/NodoController');
-
-const listaBlanca = ['*'];
 
 app.use(express.json());
 app.use(cors({
-    origin: listaBlanca
-}))
-app.get("",NodoControllers.GetNodos);
+    origin: '*'
+}));
+app.get("",(req, res) =>res.send("Api funcionando correctamente"));
 app.use("/api/v1",routes);
 
 
